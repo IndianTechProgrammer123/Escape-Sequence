df559aac7d54d4af17003cf90ff3225df078e63d
Abc_Programmer <112259649+AbcProgrammer123@users.noreply.github.com>
2022-08-28 15:10:25 +0530

    Add files via upload

---
+++ escape_sequence.py
@@ -0,0 +1,15 @@
+# Escape Sequence In Python
+# ______________________________________________________________
+# |__Escape Sequence___|____Description____|____Output__________|
+# |       \'           |   Single quote    |     '              |
+# |       \"           |   Double quote    |     "              |
+# |       \\           |   Back slash      |     \              |
+# |       \t           |   Tab             |     hello   abc    |
+# |       \n           |   New Line        |     hello          |
+# |____________________|___________________|_____123____________|
+
+print('hello\'')
+print('hello\"')
+print('hello\\')
+print('hello\tabc')
+print('hello\n123') 
